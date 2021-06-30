7f8ed7a5f1d315b15e49cc36f16496ead8c3e63d
Hugo Ribeiro <hugo.ribeiro@microcis.com.br>
2021-06-29 21:09:29 -0300

    plus count and variables

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("oie")
+	// fmt.Println("oie")
+	resultado := soma(170, 10)
+
+	fmt.Println(resultado)
+
+}
+
+func soma(a int, b int) int {
+	return a + b
 }
