8939a3c5ad7ed459f60feadfec9f00fd5720c1c5
Hugo Ribeiro <hugo.ribeiro@microcis.com.br>
2021-06-29 21:15:39 -0300

    retornando erro e capturando

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	// fmt.Println("oie")
-	resultado := soma(170, 10)
+	resultado, err := soma(-8, 10)
 
-	fmt.Println(resultado)
+	if err != nil {
+		fmt.Println("erro: " + err.Error())
+
+	} else {
+
+		fmt.Println(resultado)
+	}
 
 }
 
-func soma(a int, b int) int {
-	return a + b
+func soma(a int, b int) (int, error) {
+	if a < 0 {
+		return 0, errors.New("A é menor que 0.")
+	}
+	return a + b, nil
 }
